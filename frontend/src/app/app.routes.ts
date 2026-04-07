5559e542d6ded38860c93e882200f4c32455fa15
rauantuken06 <r_tuken@kbtu.kz>
2026-04-08 02:27:15 +0600

    fixed routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,4 +3,33 @@ import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'menu', pathMatch: 'full'},
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./pages/login/login').then(m => m.Login)
+    },
+    {
+        path:` 'register`,
+        loadComponent: () =>
+            import('./pages/register/register').then(m => m.Register)
+    },
+    {
+        path: 'menu',
+        canActivate: [authGuard],
+        loadComponent: () => 
+            import('./pages/menu/menu').then(m => m.Menu)
+    },
+    {
+        path: 'cart',
+        canActivate: [authGuard],
+        loadComponent: () => 
+            import('./pages/cart/cart').then(m => m.Cart)
+    },
+    {
+        path: 'orders',
+        canActivate: [authGuard],
+        loadComponent: () => 
+            import('./pages/orders/orders').then(m => m.Orders)
+    },
+    { path: '**', redirectTo: 'menu'}
 ];
