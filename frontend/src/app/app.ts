f976538fe9605d3c76085e4b0abed418923d0788
rauantuken06 <r_tuken@kbtu.kz>
2026-04-20 02:19:03 +0600

    added icons into header

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,16 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet, Router } from '@angular/router';
 import { mockOrders } from './dev_data/orders';
 import { Footer } from './pages/footer/footer';
+import { LucideAngularModule, ShoppingCart, PackageOpen, UserRoundCog, UserRound, User, KeyRound, SquarePlus, UtensilsCrossed } from 'lucide-angular';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, Footer],
+  imports: [CommonModule, RouterLink, RouterOutlet, Footer, LucideAngularModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  readonly ShoppingCart = ShoppingCart;
+  readonly PackageOpen = PackageOpen;
+  readonly UserRoundCog = UserRoundCog;
+  readonly UserRound = UserRound;
+  readonly KeyRound = KeyRound;
+  readonly SquarePlus = SquarePlus;
+  readonly UtensilsCrossed = UtensilsCrossed;
+
   totalItems = 0;
   totalOrders = mockOrders.length;
 
