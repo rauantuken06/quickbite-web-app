24f7c65b92d1c0bb25937e1412bc82bc25e99fd1
rauantuken06 <r_tuken@kbtu.kz>
2026-04-20 16:25:18 +0600

    smth added

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -2,15 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { LucideAngularModule, UtensilsCrossed } from 'lucide-angular';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, LucideAngularModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  readonly UtensilsCrossed = UtensilsCrossed;
+
   username = '';
   password = '';
   error = '';
